3fc91bed2e637399fa19d6d4059d1fb1fd40f1a6
Michael Kourlas <michael@kourlas.net>
2016-10-01 01:19:00 -0400

    Remove wake lock and WakefulBroadcastReceiver (not implemented properly, so not doing anything)

--- voipms-sms/src/main/java/net/kourlas/voipms_sms/receivers/BootReceiver.java
+++ voipms-sms/src/main/java/net/kourlas/voipms_sms/receivers/BootReceiver.java
@@ -17,11 +17,11 @@
 
 package net.kourlas.voipms_sms.receivers;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.content.WakefulBroadcastReceiver;
 
-public class BootReceiver extends WakefulBroadcastReceiver {
+public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
