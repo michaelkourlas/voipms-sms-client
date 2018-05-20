4f2b684ca8a913734df7ae0672b98bab88f55c57
Michael Kourlas <michael@kourlas.com>
2018-05-20 18:16:51 -0400

    Update copyright dates

--- voipms-sms/src/main/kotlin/net/kourlas/voipms_sms/ui/CustomScrollerViewProvider.kt
+++ voipms-sms/src/main/kotlin/net/kourlas/voipms_sms/ui/CustomScrollerViewProvider.kt
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 Future Mind.
- * Modifications copyright (C) 2017 Michael Kourlas
+ * Modifications copyright (C) 2017-2018 Michael Kourlas
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
