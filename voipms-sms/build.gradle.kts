cd7cc4c17c343e6fd3b8b6b7006f8f58e155fb23
Michael Kourlas <michael@kourlas.com>
2025-01-30 18:17:06 -0500

    Bump version

--- voipms-sms/build.gradle.kts
+++ voipms-sms/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "net.kourlas.voipms_sms"
         minSdk = 21
         targetSdk = 35
-        versionCode = 152
-        versionName = "0.6.31"
+        versionCode = 153
+        versionName = "0.6.32"
 
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
